18df72685ef6b5fce26de08fc2d0a05a018bb357
BMoClay <benmorgancleveland@gmail.com>
2021-01-04 17:38:03 -0500

    seed data, migrations, seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting all activities..."
+
+Activity.destroy_all
+Comment.destroy_all
+
+puts "Seeding activities..."
+10.times do
+    activity = Activity.create!(
+        title: Faker::Movie.title,
+        image_url: "https://www.postfun.com/wp-content/uploads/2019/01/Christmas-Card-Face-Swap-26100-19695.jpg"
+    )
+    5.times do
+        Comment.create!(
+            content: Faker::Movie.title
+        )
+    end
+end
