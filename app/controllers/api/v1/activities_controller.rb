d0aab0ab12660ff808d58a57d3393d8b04e84995
serkansahin55 <ssahin55@outlook.com>
2021-01-05 18:16:30 -0500

    add

---
+++ app/controllers/api/v1/activities_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::ActivitiesController < ApplicationController
+
+    def show
+        activity = Activity.find(params[:id])
+        render json: activity
+    end
+
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,3 @@
 class Comment < ApplicationRecord
+    belongs_to :activity
 end
