d13abcb39b9eeb37e74d7e7d4fc569a858a2ef45
BMoClay <benmorgancleveland@gmail.com>
2021-01-05 15:53:57 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :activities, only: [:index, :show]
+  resources :comments, only: [:create, :show, :delete]
+
 end
